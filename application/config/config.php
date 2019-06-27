bdd787be6ca418279aa3681edb046f3e16b8319a
john kevin <johnkevinbarrera@gmail.com>
2019-06-27 00:01:57 -0500

    acess

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/vacbs/';
+//$config['base_url'] = 'http://localhost/vacbs/';
 $config['base_url'] = 'http://172.22.3.169/vacbs/';
 
 /*
@@ -315,8 +315,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = 'abc';
-
+$config['encryption_key'] = 'vacbs';
 /*
 |--------------------------------------------------------------------------
 | Session Variables
